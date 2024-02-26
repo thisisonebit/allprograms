4366dedfe65b7eb9f3c45b4f616f3fd53fb203b0
surajsunil <surajsunil3369@gmail.com>
2024-02-26 21:57:46 +0530

    new

--- app/src/main/java/com/mandem/allprograms/prog_15_MultTable.java
+++ app/src/main/java/com/mandem/allprograms/prog_15_MultTable.java
@@ -8,31 +8,39 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class prog_15_MultTable extends AppCompatActivity {
+public class prog_15_MultTable extends AppCompatActivity implements View.OnClickListener {
     EditText e;
     Button b;
     TextView t;
-    int ans=0;
+    int ans = 0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_15_multitable);
-        EditText e=findViewById(R.id.no);
-        Button b=findViewById(R.id.bt);
-        TextView t=findViewById(R.id.txt);
+
+        e = findViewById(R.id.no);
+        b = findViewById(R.id.bt);
+        t = findViewById(R.id.txt);
+
         b.setOnClickListener(this);
     }
+
+    @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.bt:
-                StringBuffer sb= new StringBuffer(); int res;
-                String fs=e.getText().toString();
-                int n= Integer.parseInt(fs);
-                for (int i=1;i<=10;i++)
-                {
-                    ans=(i*n);
-                    sb.append(i +"X"+n+"="+ans+"\n\n"); }
+                StringBuffer sb = new StringBuffer();
+                int res;
+                String fs = e.getText().toString();
+                int n = Integer.parseInt(fs);
+                for (int i = 1; i <= 10; i++) {
+                    ans = (i * n);
+                    sb.append(i + "X" + n + "=" + ans + "\n\n");
+                }
                 t.setText(sb);
-                break; }}
+                break;
+        }
+    }
 }
+
